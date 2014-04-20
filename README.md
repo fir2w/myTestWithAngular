bdc63da7ea7c48e75e84d45169c61adec2337382
wangwei <wangwei_fir@126.com>
2014-04-20 22:20:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+angularFirst
+============
+
+一个空的angular项目，只有简单的demo和构建文件，用来作为其他项目的根基
+
+grunt server
+
+访问：`http://127.0.0.1:9000/#/`
---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('firstApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+   // $locationProvider.html5Mode(true);
+  });
